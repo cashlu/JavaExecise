908b54b4562a7625569c8928cf577a3baa9426b0
cashlu <cashlu@yeah.net>
2015-03-25 01:24:02 +0800

    Nothing

--- src/testio/TextFileCopy.java
+++ src/testio/TextFileCopy.java
@@ -9,21 +9,30 @@ import java.io.*;
 public class TextFileCopy {
     private File sour;
     private File dest;
+    private boolean isAppend;
 
-    public void copy(File s, File d) {
+    /**
+     * 文件拷贝方法。
+     * @param s 源文件
+     * @param d 目标文件，不存在则创建
+     * @param append 是否追加拷贝，如果该参数为true，并且该目标文件存在时，则向目标文件结尾追加拷贝。
+     */
+    public void copy(File s, File d, boolean append) {
         this.sour = s;
         this.dest = d;
+        this.isAppend = append;
+
         FileReader fr = null;
         FileWriter fw = null;
 
         //Begin to read the content of source file
         try {
             fr = new FileReader(s);
-            fw = new FileWriter(d);
+            fw = new FileWriter(d, isAppend);
             char[] buffer = new char[1024];
-            int num = 0;
-            while ((num = fr.read(buffer)) != -1) {
-                System.out.print(new String(buffer, 0, num));
+            int len = 0;
+            while ((len = fr.read(buffer)) != -1) {
+                System.out.print(new String(buffer, 0, len));
                 //write the content into destination file
                 fw.write(buffer);
             }
@@ -46,9 +55,11 @@ public class TextFileCopy {
     public static void main(String[] args) {
         File s = new File("/Users/cashlu/Desktop/demo.txt");
         File d = new File("/Users/cashlu/Desktop/demoTo.txt");
+        File as = new File("/Users/cashlu/Desktop/湟源县农牧局通讯录导入SQL.txt");
 
         TextFileCopy t = new TextFileCopy();
-        t.copy(s, d);
+        t.copy(s, d, false);
+        t.copy(as, d, true);
 
     }
 }
